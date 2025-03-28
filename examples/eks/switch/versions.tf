a9daea74140bd9c527dbaf92d6b051b8a32070ef
mohzim <mohzim@gmail.com>
2025-03-28 19:35:59 +0530

    Add backend for tfstate in S3

--- examples/eks/switch/versions.tf
+++ examples/eks/switch/versions.tf
@@ -1,4 +1,10 @@
 terraform {
+   backend "s3" {
+    bucket = "mohzim-terraform"
+    key    = "terraform-aws-scheduled-switch-parent-tfstate"
+    region = "ap-south-1"
+  }
+
   required_version = ">= 1.1.0"
 
   required_providers {
